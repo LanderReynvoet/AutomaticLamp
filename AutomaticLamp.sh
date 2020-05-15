91303d0311c90a80bc04d7d42888d35ccda958a3
Robin Van der Gucht <robin.van.der.gucht@student.howest.be>
2020-05-15 10:42:21 +0200

    Adding php-zip

--- AutomaticLamp.sh
+++ AutomaticLamp.sh
@@ -76,6 +76,7 @@ apt install openssl -y
 apt install unzip -y 
 apt install php-imagick php-phpseclib php-php-gettext php7.3-common php7.3-mysql php7.3-gd php7.3-imap php7.3-json php7.3-curl php7.3-zip php7.3-xml php7.3-mbstring php7.3-bz2 php7.3-intl php7.3-gmp -y #required php modules for phpmyadmin
 apt install php-dom php-mbstring -y #required for laravel 
+apt install php-zip -y #required for composer
 apt install sudo
 echo -e "\e[1;92mAll necessary dependencies installed\e[0m"
 	}
